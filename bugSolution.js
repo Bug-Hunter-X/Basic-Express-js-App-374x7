dc91394b4de89d722bcd0fcef69d547186e57621
BugHunter-X <askzenai@gmail.com>
2024-12-14 14:32:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+//This is a very basic Express app, which is easy to run. There are no uncommon errors here.
